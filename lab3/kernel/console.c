f665983173ed4fa2953cfaf54d9e43edf960d3ca
NIL-zhuang <181830266@smail.nju.edu.cn>
2020-11-30 10:28:12 +0800

    区分tab和4个空格

--- lab3/kernel/console.c
+++ lab3/kernel/console.c
@@ -150,7 +150,7 @@ PUBLIC void out_char(CONSOLE* p_con, char ch) {
             if (p_con->cursor < p_con->original_addr + p_con->v_mem_limit - 4) {
                 int pos = (int)(p_con->cursor - p_con->original_addr);
                 for (int i = 0; i < 4; i++) {
-                    *p_vmem++ = ' ';
+                    *p_vmem++ = '\0';
                     *p_vmem++ = DEFAULT_CHAR_COLOR;
                     p_con->cursor++;
                     pos_record[pos++] = '\t';
