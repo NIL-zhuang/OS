9d9fc72a37bafcd437087fcc0a36bc9818e17425
NIL-zhuang <181830266@smail.nju.edu.cn>
2020-12-24 10:32:56 +0800

    添加了printf(%c)的功能

--- lab4/kernel/vsprintf.c
+++ lab4/kernel/vsprintf.c
@@ -21,6 +21,7 @@ int vsprintf(char *buf, const char *fmt, va_list args) {
     char tmp[256];
     va_list p_next_arg = args;
 
+    // 将fmt里的内容拷贝到buf里，如果遇到%则解析%后的内容并重新导入
     for (p = buf; *fmt; fmt++) {
         if (*fmt != '%') {
             *p++ = *fmt;
@@ -35,6 +36,11 @@ int vsprintf(char *buf, const char *fmt, va_list args) {
                 strcpy(p, tmp);
                 p_next_arg += 4;
                 p += strlen(tmp);
+                break;
+            case 'c':
+                // 解析一个char字符
+                *p++ = *((char *)p_next_arg);
+                p_next_arg += 4;
                 break;
             case 's':
                 break;
